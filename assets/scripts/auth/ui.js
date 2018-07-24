d6a4c083e996d963686389c6c95f2b3b930ff3a1
CoreyJNash <CoreyJNash@gmail.com>
2018-07-24 14:52:25 -0400

    Fixed authentication path

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -3,20 +3,20 @@
 const store = require('../store')
 
 const signUpSuccess = function (data) {
-  $('#message').text('Signed up successfully')
-  $('#message').css('background-color', ' #f7bd2f')
-  $('#message').css('font-size', '20px')
-  $('#message').css('text-align', 'center')
-  $('#sign-up')[0].reset()
-  // console.log('signUpSuccess ran. Data is :', data)
+//   $('#message').text('Signed up successfully')
+//   $('#message').css('background-color', ' #f7bd2f')
+//   $('#message').css('font-size', '20px')
+//   $('#message').css('text-align', 'center')
+//   $('#sign-up')[0].reset()
+  console.log('signUpSuccess ran. Data is :', data)
   
 }
 
 const signUpFailure = function (error) {
-  $('#message').text('Error on sign up')
-  $('#message').css('background-color', 'red')
-  $('#message').css('text-align', 'center')
-  // console.error('signUpFailure ran. Error is :', error)
+//   $('#message').text('Error on sign up')
+//   $('#message').css('background-color', 'red')
+//   $('#message').css('text-align', 'center')
+  console.error('signUpFailure ran. Error is :', error)
 }
 
 const signInSuccess = function (data) {
@@ -24,10 +24,10 @@ const signInSuccess = function (data) {
 //   $('#message').css('background-color', ' #f7bd2f')
 //   $('#message').css('font-size', '20px')
 //   $('#message').css('text-align', 'center')
-//   $('#sign-out').removeClass('hidden')
-// //   $('#change-password').removeClass('hidden')
-//   $('#sign-in').addClass('hidden')
-//   $('#sign-up').addClass('hidden')
+  $('#sign-out').removeClass('hidden')
+  $('#change-password').removeClass('hidden')
+  $('#sign-in').addClass('hidden')
+  $('#sign-up').addClass('hidden')
 //   $('#message').css('timeout', 6000)
 //   $('#sign-in')[0].reset()
   console.log('signInSuccess ran. Data is :', data)
@@ -35,10 +35,10 @@ const signInSuccess = function (data) {
 }
 
 const signInFailure = function (error) {
-  $('#message').text('Error on sign in')
-  $('#message').css('background-color', 'red')
-  $('#message').css('text-align', 'center')
-  // console.error('signInFailure ran. Error is :', error)
+//   $('#message').text('Error on sign in')
+//   $('#message').css('background-color', 'red')
+//   $('#message').css('text-align', 'center')
+  console.error('signInFailure ran. Error is :', error)
 }
 
 const signOutSuccess = function () {
@@ -49,28 +49,28 @@ const signOutSuccess = function () {
   $('#sign-in').removeClass('hidden')
   $('#change-password').addClass('hidden')
   $('#sign-out').addClass('hidden')
-  $('#view-games').addClass('hidden')
-  // console.log('signOutSuccess ran and nothing was returned!')
+//   $('#view-games').addClass('hidden')
+  console.log('signOutSuccess ran and nothing was returned!')
   store.user = null
 }
 
 const signOutFailure = function (error) {
-  $('#message').text('Error on sign out')
-  $('#message').css('background-color', 'red')
-  $('#message').css('text-align', 'center')
-  // console.error('signOutFailure ran. Error is :', error)
+//   $('#message').text('Error on sign out')
+//   $('#message').css('background-color', 'red')
+//   $('#message').css('text-align', 'center')
+  console.error('signOutFailure ran. Error is :', error)
 }
 
 const changePasswordSuccess = function () {
-  $('#message').text('Changed password successfully')
-  $('#message').css('background-color', '#f7bd2f')
-  // console.log('changePasswordSuccess ran and nothing was returned!')
+//   $('#message').text('Changed password successfully')
+//   $('#message').css('background-color', '#f7bd2f')
+  console.log('changePasswordSuccess ran and nothing was returned!')
 }
 
 const changePasswordFailure = function (error) {
-  $('#message').text('Error on change password')
-  $('#message').css('background-color', 'red')
-  // console.error('changePasswordFailure ran. Error is :', error)
+//   $('#message').text('Error on change password')
+// //   $('#message').css('background-color', 'red')
+console.error('changePasswordFailure ran. Error is :', error)
 }
 
 module.exports = {
