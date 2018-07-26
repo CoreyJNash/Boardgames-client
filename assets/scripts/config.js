2cb183eeb2158ee28348cad807a8ba293efada51
CoreyJNash <CoreyJNash@gmail.com>
2018-07-25 23:40:38 -0400

    fixed API url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://dashboard.heroku.com/apps/gentle-temple-31740',
+  production: 'https://gentle-temple-31740.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
