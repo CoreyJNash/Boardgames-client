668c65384e9ee9a333a0c0d04e979c677d4db3b9
CoreyJNash <CoreyJNash@gmail.com>
2018-07-25 23:23:16 -0400

    added API url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://dashboard.heroku.com/apps/gentle-temple-31740',
   development: 'http://localhost:4741'
 }
 
